9f3a9bea942c8850f9e0b0a070e446bafa7c6705
Lezhi Wang <lezhi@ad.unc.edu>
2025-01-18 21:42:19 -0500

    Lecture note

---
+++ class.py
@@ -0,0 +1,9 @@
+def perimeter(length: float, width: float) -> float:
+    return 2 * length + 2 * width
+
+
+print(perimeter(length=10.0, width=8.0))
+
+print(perimeter(length=5.0, width=8.0))
+
+"fox"[1]
