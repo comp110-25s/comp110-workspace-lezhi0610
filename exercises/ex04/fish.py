d9a8e3d9b605d18f7156cd695b31a89fd9a16a4a
Lezhi Wang <lezhi@ad.unc.edu>
2025-04-08 16:27:33 -0400

    3rd upload

--- exercises/ex04/fish.py
+++ exercises/ex04/fish.py
@@ -4,10 +4,14 @@ __author__ = "730702700"
 
 
 class Fish:
+    """Class to define Fish"""
+
     age: int
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """New fish with age 0"""
         self.age = 0
 
-    def one_day(self):
+    def one_day(self) -> None:
+        """Fish ages"""
         self.age += 1
--- exercises/ex04/river.py
+++ exercises/ex04/river.py
@@ -3,16 +3,18 @@
 __author__ = "730702700"
 
 
-from .fish import Fish
-from .bear import Bear
+from exercises.ex04.fish import Fish
+from exercises.ex04.bear import Bear
 
 
 class River:
+    """Class to define River"""
+
     day: int
     bears: list[Bear]
     fish: list[Fish]
 
-    def __init__(self, num_fish: int, num_bears: int):
+    def __init__(self, num_fish: int, num_bears: int) -> None:
         """New River with num_fish Fish and num_bears Bears"""
         self.day: int = 0
         self.fish: list[Fish] = []
@@ -23,25 +25,25 @@ class River:
         for _ in range(0, num_bears):
             self.bears.append(Bear())
 
-    def check_ages(self):
+    def check_ages(self) -> None:
         """Remove old Fish and Bear's from River"""
         fish1 = [fish for fish in self.fish if fish.age <= 3]
         bears1 = [bear for bear in self.bears if bear.age <= 5]
         self.fish = fish1
         self.bears = bears1
 
-    def remove_fish(self, amount: int):
+    def remove_fish(self, amount: int) -> None:
         """Remove fish from river when Bears eat"""
         self.fish = self.fish[amount:]
 
-    def bears_eating(self):
+    def bears_eating(self) -> None:
         """Remove fish from river when Bears eat"""
         for bear in self.bears:
             if len(self.fish) >= 5:
                 self.remove_fish(3)
                 bear.eat(3)
 
-    def check_hunger(self):
+    def check_hunger(self) -> None:
         """Remove hungry Bears from River"""
         bears2 = []
         for bear in self.bears:
@@ -49,25 +51,25 @@ class River:
                 bears2.append(bear)
         self.bears = bears2
 
-    def repopulate_fish(self):
+    def repopulate_fish(self) -> None:
         """Repopulate the fish in the river"""
         num_new_fish = (len(self.fish) // 2) * 4
         for _ in range(num_new_fish):
             self.fish.append(Fish())
 
-    def repopulate_bears(self):
+    def repopulate_bears(self) -> None:
         """Repopulate the bears in the river"""
         num_new_bears = len(self.bears) // 2
         for _ in range(num_new_bears):
             self.bears.append(Bear())
 
-    def view_river(self):
+    def view_river(self) -> None:
         """Visualize the river"""
         print(f"~~~ Day {self.day}: ~~~")
         print(f"Fish population: {len(self.fish)}")
         print(f"Bear population: {len(self.bears)}")
 
-    def one_river_day(self):
+    def one_river_day(self) -> None:
         """Simulate one day of life in the river"""
         # Increase day by 1
         self.day += 1
@@ -90,7 +92,7 @@ class River:
         # Visualize River
         self.view_river()
 
-    def one_river_week(self):
+    def one_river_week(self) -> None:
         """Simulate one week of life in the river"""
         for _ in range(7):
             self.one_river_day()
