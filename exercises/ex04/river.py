1dbe2c59e15aa6a238ee3325f6817121dc1dc200
Lezhi Wang <lezhi@ad.unc.edu>
2025-04-08 15:57:15 -0400

    EX04 finish!

--- exercises/ex04/river.py
+++ exercises/ex04/river.py
@@ -3,9 +3,13 @@
 from exercises.EX04.fish import Fish
 from exercises.EX04.bear import Bear
 
+
 class River:
-    
-    def __init__(self, num_fish: int, num_bears:int):
+    day: int
+    bears: list[Bear]
+    fish: list[Fish]
+
+    def __init__(self, num_fish: int, num_bears: int):
         """New River with num_fish Fish and num_bears Bears"""
         self.day: int = 0
         self.fish: list[Fish] = []
@@ -17,23 +21,42 @@ class River:
             self.bears.append(Bear())
 
     def check_ages(self):
-        return None
+        fish1 = [fish for fish in self.fish if fish.age <= 3]
+        bears1 = [bear for bear in self.bears if bear.age <= 5]
+        self.fish = fish1
+        self.bears = bears1
+
+    def remove_fish(self, amount: int):
+        self.fish = self.fish[amount:]
 
     def bears_eating(self):
-        return None
-    
+        for bear in self.bears:
+            if len(self.fish) >= 5:
+                self.remove_fish(3)
+                bear.eat(3)
+
     def check_hunger(self):
-        return None
-        
+        bears2 = []
+        for bear in self.bears:
+            if bear.hunger_score >= 0:
+                bears2.append(bear)
+        self.bears = bears2
+
     def repopulate_fish(self):
-        return None
-    
+        num_new_fish = (len(self.fish) // 2) * 4
+        for _ in range(num_new_fish):
+            self.fish.append(Fish())
+
     def repopulate_bears(self):
-        return None
-    
+        num_new_bears = len(self.bears) // 2
+        for _ in range(num_new_bears):
+            self.bears.append(Bear())
+
     def view_river(self):
-        return None
-            
+        print(f"~~~ Day {self.day}: ~~~")
+        print(f"Fish population: {len(self.fish)}")
+        print(f"Bear population: {len(self.bears)}")
+
     def one_river_day(self):
         """Simulate one day of life in the river"""
         # Increase day by 1
@@ -56,4 +79,7 @@ class River:
         self.repopulate_bears()
         # Visualize River
         self.view_river()
-            
+
+    def one_river_week(self):
+        for _ in range(7):
+            self.one_river_day()
--- exercises/ex04/river_simulation.py
+++ exercises/ex04/river_simulation.py
@@ -1,1 +1,5 @@
+from exercises.EX04.river import River
 
+my_river = River(10, 2)
+
+my_river.view_river()
